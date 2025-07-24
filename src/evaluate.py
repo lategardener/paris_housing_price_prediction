a8aaa3510dc92a05f3ce6ebc57568bec2505da2b
lategardener <djolemarc@cy-tech.fr>
2025-07-24 20:35:05 +0200

    adjust file

--- src/evaluate.py
+++ src/evaluate.py
@@ -3,12 +3,11 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 def evaluation(y_true, y_pred, metric):
     if metric == 'mse':
         return mean_squared_error(y_true, y_pred)
-    elif metric == 'mae':
+    if metric == 'mae':
         return mean_absolute_error(y_true, y_pred)
-    elif metric == 'r2':
+    if metric == 'r2':
         return r2_score(y_true, y_pred)
-    else:
-        return None
+    return None
 
 def run_all_metrics(y_true, y_pred, model_name=None):
     regression_mae = evaluation(y_true, y_pred, "mae")
@@ -20,3 +19,4 @@ def run_all_metrics(y_true, y_pred, model_name=None):
     print(f"🔹 MAE (Mean Absolute Error) : {regression_mae:.2f}")
     print(f"🔹 MSE (Mean Squared Error)  : {regression_mse:.2f}")
     print(f"🔹 R2  (Coefficient of Determination)  : {regression_r2:.2f}")
+
